2adc32991b9d539c135affa6e91e23b734bc817d
Jordan Proctor <jordanmarieproctor@gmail.com>
2021-05-02 23:31:04 -0500

    ex5

--- ViewUsers.php
+++ ViewUsers.php
@@ -4,7 +4,7 @@ if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit(); }
     else {
-			$query = "SELECT user_id FROM Users ORDER BY user_id ASC";
+			$query = "SELECT user_id FROM Users ORDER BY user_id";
 			if ($result = $mysqli->query($query)) {
 				while ($row = $result->fetch_assoc()) {
 					echo "<br>" . $row["user_id"] . "<br>";
